bee23a4274248003b142e3430e60c884120842a9
rebs-code <jack.rebussi@gmail.com>
2023-11-09 15:14:44 +0100

    Add button to roll die and implement game logic in main.js

--- Esercizio-2/js/main.js
+++ Esercizio-2/js/main.js
@@ -1,1 +1,16 @@
 "use strict";
+
+function rollDie() {
+    const userResult = Math.floor(Math.random() * 6) + 1;
+    const pcResult = Math.floor(Math.random() * 6) + 1;
+    console.log(`You rolled a ${userResult}`);
+    console.log(`Computer rolled a ${pcResult}`);
+
+    if (userResult > pcResult) {
+        console.log("User vince")
+    } else if (userResult === pcResult) {
+        console.log("Pareggio!")
+    } else {
+        console.log("Computer vince")
+    };
+};
